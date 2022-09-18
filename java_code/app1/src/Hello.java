5d50e6a6d7868f0bb5298530950e505e861535ee
xujunze <xujunze_97@163.com>
2022-09-18 18:24:03 +0800

    modified:   README.md new file:   java_code/app1/src/ArraysTest.java new file:   java_code/app1/src/ArraysTest2.java new file:   java_code/app1/src/Assign.java new file:   java_code/app1/src/Hello.java new file:   java_code/app1/src/ListExample.java new file:   java_code/app1/src/MyDate.java new file:   java_code/app1/src/MyDateTest.java new file:   java_code/app1/src/SetExample.java renamed:    day1/src/one.java -> java_code/day1/src/one.java renamed:    day1/src/two.java -> java_code/day1/src/two.java renamed:    day2/src/Person.java -> java_code/day2/src/Person.java renamed:    day2/src/Phone.java -> java_code/day2/src/Phone.java renamed:    day2/src/SuperMarket/Goods.java -> java_code/day2/src/SuperMarket/Goods.java renamed:    day2/src/SuperMarket/Person.java -> java_code/day2/src/SuperMarket/Person.java renamed:    day2/src/SuperMarket/SuperMarket.java -> java_code/day2/src/SuperMarket/SuperMarket.java renamed:    day2/src/SuperMarket/Test.java -> java_code/day2/src/SuperMarket/Test.java renamed:    day2/src/Test_Person.java -> java_code/day2/src/Test_Person.java renamed:    day2/src/Testphone.java -> java_code/day2/src/Testphone.java renamed:    day2/src/first.java -> java_code/day2/src/first.java renamed:    day2/src/object1.java -> java_code/day2/src/object1.java renamed:    day2/src/object2.java -> java_code/day2/src/object2.java renamed:    day3/src/Animal.java -> java_code/day3/src/Animal.java renamed:    day3/src/AnimalTest.java -> java_code/day3/src/AnimalTest.java renamed:    day3/src/Animal_abstract/Animal.java -> java_code/day3/src/Animal_abstract/Animal.java renamed:    day3/src/Animal_abstract/AnimalTest.java -> java_code/day3/src/Animal_abstract/AnimalTest.java renamed:    day3/src/Animal_abstract/Cat.java -> java_code/day3/src/Animal_abstract/Cat.java renamed:    day3/src/Animal_abstract/Dog.java -> java_code/day3/src/Animal_abstract/Dog.java renamed:    day3/src/Animal_abstract/DogAnimal.java -> java_code/day3/src/Animal_abstract/DogAnimal.java renamed:    day3/src/Animal_interface/Animal.java -> java_code/day3/src/Animal_interface/Animal.java renamed:    day3/src/Animal_interface/AnimalTest.java -> java_code/day3/src/Animal_interface/AnimalTest.java renamed:    day3/src/Animal_interface/Cat.java -> java_code/day3/src/Animal_interface/Cat.java renamed:    day3/src/Animal_interface/Dog.java -> java_code/day3/src/Animal_interface/Dog.java renamed:    day3/src/Cat.java -> java_code/day3/src/Cat.java renamed:    day3/src/CatTest.java -> java_code/day3/src/CatTest.java renamed:    day3/src/Dog.java -> java_code/day3/src/Dog.java renamed:    day3/src/DogTest.java -> java_code/day3/src/DogTest.java renamed:    day3/src/Tools.java -> java_code/day3/src/Tools.java renamed:    day3/src/ToolsHuawei.java -> java_code/day3/src/ToolsHuawei.java renamed:    day3/src/Transportation/Express.java -> java_code/day3/src/Transportation/Express.java renamed:    day3/src/Transportation/GPS.java -> java_code/day3/src/Transportation/GPS.java renamed:    day3/src/Transportation/Test.java -> java_code/day3/src/Transportation/Test.java renamed:    day3/src/Transportation/Tool.java -> java_code/day3/src/Transportation/Tool.java renamed:    day3/src/Transportation/Truck.java -> java_code/day3/src/Transportation/Truck.java renamed:    day4/src/ArringListTest.java -> java_code/day4/src/ArringListTest.java renamed:    day4/src/WeiboLoginDemo/LoginTest.java -> java_code/day4/src/WeiboLoginDemo/LoginTest.java renamed:    day4/src/WeiboLoginDemo/User.java -> java_code/day4/src/WeiboLoginDemo/User.java renamed:    day4/src/WeiboLoginDemo/UserList.java -> java_code/day4/src/WeiboLoginDemo/UserList.java
    anges not staged for commit:
    deleted:    app1/src/ArraysTest.java
    deleted:    app1/src/ArraysTest2.java
    deleted:    app1/src/Assign.java
    deleted:    app1/src/Hello.java
    deleted:    app1/src/ListExample.java
    deleted:    app1/src/MyDate.java
    deleted:    app1/src/MyDateTest.java
    deleted:    app1/src/SetExample.java

---
+++ java_code/app1/src/Hello.java
@@ -0,0 +1,12 @@
+/*
+     简单的 HelloWorld 程序
+*/
+public class Hello
+{
+    // main方法
+    public static void main(String args[])
+    {
+        System.out.println("Hello World!");
+        // 输出字符串“Hello World!”
+    }
+}
